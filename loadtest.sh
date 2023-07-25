b485f28a87d65d93b0ba2a8344f0f0bcb0977c90
loc trinh <trphucloc@gmail.com>
2023-07-25 09:53:25 +0700

    update

--- loadtest.sh
+++ loadtest.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-locust -f locustfile.py --host https://azure-devops-project2-khuongph.azurewebsites.net/ --users 500 --spawn-rate 5 
+locust -f locustfile.py --host https://azure-devops-project2-loc.azurewebsites.net/ --users 500 --spawn-rate 5 
     
